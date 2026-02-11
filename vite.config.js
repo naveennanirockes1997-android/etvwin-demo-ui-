ea09de24976e59867d674939346b7e883b65f58b
naveennanirockes1997-android <naveennanirockes1997@gmail.com>
2026-02-11 15:36:17 +0530

    second message

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/etv-win-ui/",
+  base:"//naveennanirockes1997-android.github.io/etvwin-demo-ui-/",
   plugins: [react()],
   server: {
     proxy: {
